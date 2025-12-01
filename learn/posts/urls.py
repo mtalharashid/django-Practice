62f99ffdd3b9c29e86a6047cfcfbe8070ad9b15c
Talha Rashid <mtr04412@gmail.com>
2025-12-01 17:52:48 +0400

    Authentication and create logout form and login page

--- learn/posts/urls.py
+++ learn/posts/urls.py
@@ -6,5 +6,6 @@ app_name = "posts"
 
 urlpatterns = [
     path("", views.posts_list, name="list"),
+    path("new-post/", views.posts_new, name="new-post"),
     path("<slug:slug>", views.post_page, name="page"),
 ]
