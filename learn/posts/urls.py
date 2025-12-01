3f81961a0623eb141f102a0dce62ec403312d22d
Talha Rashid <mtr04412@gmail.com>
2025-12-01 20:55:42 +0400

    Add a new post
    Django Custom Forms

--- learn/posts/urls.py
+++ learn/posts/urls.py
@@ -6,6 +6,6 @@ app_name = "posts"
 
 urlpatterns = [
     path("", views.posts_list, name="list"),
-    path("new-post/", views.posts_new, name="new-post"),
+    path("new-post/", views.new_post, name="new-post"),
     path("<slug:slug>", views.post_page, name="page"),
 ]
--- learn/posts/views.py
+++ learn/posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from . import forms
 # Create your views here.
 
 def posts_list(request):
@@ -12,5 +13,14 @@ def post_page(request, slug):
     return render(request, "posts/post_page.html", { "post": post })
 
 @login_required(login_url="/users/login")
-def posts_new(request):
-    return render(request, "posts/post_new.html")
+def new_post(request):
+    if request.method == "POST":
+        form = forms.createPost(request.POST, request.FILES)
+        if form.is_valid():
+            newpost = form.save(commit=False)
+            newpost.auther = request.user
+            newpost.save()
+            return redirect('posts:list')
+    else:
+        form = forms.createPost()
+    return render(request, "posts/post_new.html", { "form": form })
